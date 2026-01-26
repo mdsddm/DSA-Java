d3c534c8390d101a293611a764eabf03262ad5ff
mdsddm <md.saddam.gtm@gmail.com>
2026-01-26 22:59:10 +0530

    nothing

--- BinarySearch/PeakIndexInMountainArray.java
+++ BinarySearch/PeakIndexInMountainArray.java
@@ -1,4 +1,5 @@
 package BinarySearch;
+
 /*
 852. Peak Index in a Mountain Array.
 You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
@@ -19,18 +20,20 @@ Output: 1
  */
 public class PeakIndexInMountainArray {
     public static void main(String[] args) {
-        int[] arr={0,10,5,2};
+        int[] arr = { 0, 10, 5, 2 };
         System.out.println(peakIndexInMountainArray(arr));
     }
+
     public static int peakIndexInMountainArray(int[] arr) {
-        int low=0,high=arr.length-1;
+        int low = 0, high = arr.length - 1;
         int mid;
-        while (low<high){
-            mid=low+(high-low)/2;
-            if(arr[mid]>arr[mid+1])
-                high=mid;
+        while (low < high) {
+            mid = low + (high - low) / 2;
+            if (arr[mid] > arr[mid + 1])
+                high = mid;
             else
-                low=mid+1;  }
+                low = mid + 1;
+        }
         return high;
     }
 }
--- TwoPointerAlgorithm/TwoSum.java
+++ TwoPointerAlgorithm/TwoSum.java
@@ -23,28 +23,28 @@ Output: [0,1]
  */
 public class TwoSum {
     public static void main(String[] args) {
-        int[] nums ={2,7,11,15};
-        int [] ans =twoSum(nums,9);
+        int[] nums = { 2, 7, 11, 15 };
+        int[] ans = twoSum(nums, 9);
         System.out.println(Arrays.toString(ans));
     }
+
     public static int[] twoSum(int[] nums, int target) {
-        int lp=0,rp=nums.length-1;
-        while (rp>lp){
-            if(nums[lp]+nums[rp]==target){
-                return new int[] {lp,rp};   }
-            else if (nums[lp]+nums[rp]>target){
+        int lp = 0, rp = nums.length - 1;
+        while (rp > lp) {
+            if (nums[lp] + nums[rp] == target) {
+                return new int[] { lp, rp };
+            } else if (nums[lp] + nums[rp] > target) {
                 rp--;
-            }
-            else{
+            } else {
                 lp++;
             }
         }
-        for (int i=0;i<nums.length-1;i++){
-            for (int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target)
-                    return new int[] {i,j};
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target)
+                    return new int[] { i, j };
             }
         }
-        return new int[] {-1,-1};
+        return new int[] { -1, -1 };
     }
 }
